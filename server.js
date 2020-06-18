485fd5965d15ee2907b1819c4691e49031833d79
Chris-Bortel <bortelcc19@gmail.com>
2020-06-18 15:19:39 -0700

    basic server is built

--- server.js
+++ server.js
@@ -18,13 +18,27 @@ app.use(cors()); //lets express function use other stuff...Dont worry about what
 
 //Route on the "/" in the url bar of the browser
 //request and response are the parameters. Response: has methods that send data
-app.get("/", (request, response) => {
+app.get("/location", (request, response) => {
   response.send("OK, you got it");
 });
 
 app.get("/person", (request, response) => {
   let person = { name: "chris", cool: true };
   response.json(person);
+});
+
+app.get("/bad", (request, response) => {
+  throw new Error("What is going on!");
+});
+//app.put(), app.delete(), app.post()
+app.use("*", (request, response) => {
+  //this is a catch all, will make it so your server does not break
+  response.status(404).send(" what are you trying to do?");
+});
+
+//500 is bad news... app.use with a for parameter thing is an error handler
+app.use((error, request, response, next) => {
+  response.status(500).send(" server is broken");
 });
 
 app.listen(PORT, () => console.log("Server running on port", PORT));
