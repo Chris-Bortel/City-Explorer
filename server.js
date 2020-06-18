f4746ff7120ccb0153da63378db80e2268f3de9e
Chris-Bortel <bortelcc19@gmail.com>
2020-06-18 14:12:04 -0700

    console is reading my port value

---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// dotenv, expres, cors
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+// console.log('hello');
+// this is the magic that john was talking about
+console.log(process.env.PORT);
+
+//handle a request for location data
+//get a city from the client
+//ffetch data from an API
+//adapt the data, usign a Constructor function
+//send the adapted data to the client
+
+// locatopn constructor function
+// tanke is som big object, turn it into something that matches the contract
+
+//handle a request for retaurant data
+// get location info from the client (lat, logn, city-name)
+//fetch data from an api
+//adapt the data, using a contructor function
