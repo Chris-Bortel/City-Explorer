2f81a4a76d614d4f9eb99d717b77edb361455eaf
Chris-Bortel <bortelcc19@gmail.com>
2020-06-27 18:49:52 -0700

    handleLocation function has the sql statements. I am working on the locations data cache.

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS locations;
 
 CREATE TABLE locations (
   id SERIAL PRIMARY KEY,
-  formatted_query VARCHAR(255),
   search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
   latitude VARCHAR(255),
   longitude VARCHAR(255)
 );
--- server.js
+++ server.js
@@ -8,13 +8,9 @@ const cors = require("cors");
 const pg = require("pg");
 const superagent = require("superagent");
 
-// this references the .env file and spits out the port
 const PORT = process.env.PORT || 3000;
-// Initializes an express server
 const app = express();
 const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client(process.env.POSTGRES);
-// tells server to use the cors library the () = everyone //Cors limits who can access your server--- cors is the key that unlocks the server so that we can ask for data
 app.use(cors());
 
 // Routes
@@ -23,76 +19,97 @@ app.get("/location", handleLocation);
 app.get("/weather", handleWeather);
 app.get("/trails", handleTrails);
 
+// Start the server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Promise to the server that the client is connected
+client
+  .connect()
+  .then(() => console.log("client is connected"))
+  .catch((err) => {
+    throw `PG startup error: ${err.message}`; // TODO: this is undefined?
+  });
+
 app.get("/add", (request, response) => {
   // get data from front end
   console.log(request.query.potatoes); // query is a property withing the request object which is given from the callback function for this route.
-  const latitudeQuery = request.query.latitude;
-
-  const longitudeQuery = request.query.longitude;
-  const formattedQuery = request.query.formatted_query;
-  const searchQuery = request.query.search_query;
-  const safeQuery = [
-    latitudeQuery,
-    longitudeQuery,
-    formattedQuery,
-    searchQuery,
-  ];
+  // const searchQuery = request.query.search_query;
+  // const formattedQuery = request.query.formatted_query;
+  // const latitudeQuery = request.query.latitude;
+  // const longitudeQuery = request.query.longitude;
+  // const safeQuery = [
+  //   latitudeQuery,
+  //   longitudeQuery,
+  //   formattedQuery,
+  //   searchQuery,
+  // ];
   // This
   // http://localhost:3000/add?latitude=47.8279&longitude=-122.3054&formatted_query=seattle&search_query=seattle
   // need to ma
-  // Sql query
+  // Sql query line 86-98
   const SQL =
-    "INSERT INTO locations (latitude, longitude, formatted_query, search_query) VALUES ($1, $2, $3, $4);"; // first arguement, second arg, etc.
+    "INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *;"; // first arguement, second arg, etc.
 
   // give SQL query to pg agent --- go to db and make request
-  client
-    .query(SQL, safeQuery)
-    .then((results) => {
-      console.log(results);
-      // look in results for rowCount // that should give me a one,. which tells us that my insert statement worked
-      response.status(200).json(results);
-    })
-    .catch((error) => {
-      response.status(500).send(error);
-    });
+  // client
+  //   .query(SQL, safeQuery)
+  //   .then((results) => {
+  //     console.log(results);
+  //     // look in results for rowCount // that should give me a one,. which tells us that my insert statement worked
+  //     response.status(200).json(results);
+  //   })
+  //   .catch((error) => {
+  //     response.status(500).send(error);
+  //   });
 });
-
-// Start the server
-client
-  .connect()
-  .then(() => {
-    // a promise that the server will not start without first connecting to the database
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => {
-    throw `PG startup error: ${err.message}`;
-  });
-
-// Route Handlers
-
-function handleHomePage(request, response) {
-  response.send(`PORT ${PORT} is running`);
-}
 
 // In Memory Cache
 let locations = {};
 
-function handleLocation(request, response) {
-  // request.query.city is what the user typed in...
-  // If the database has it ...
-  if (locations[request.query.city]) {
-    console.log("we have it already...");
-    response.status(200).send(locations[request.query.city]);
-  } else {
-    console.log("going to get it");
-    fetchLocationDataFromAPI(request.query.city, response);
-  }
+// Route Handlers
+function handleHomePage(request, response) {
+  response.send(`PORT ${PORT} is running`);
 }
 
-function fetchLocationDataFromAPI(city, response) {
+function handleLocation(request, response) {
+  // request.query.city is what the user typed in...
+  // check if the database has it with a select statement
+  //figure out how I change select statement to find just the row for tacoma
+  const SQL = "SELECT * FROM locations WHERE search_query = $1";
+  //???????/ Am I wanting to have this be the safe query variable? ????????
+  const city = [request.query.city];
+  // then inside of my .then ----- that is when I do the if statement
+  /////Use .rowcount to return rows affected by the last executed statement
+  client
+    .query(SQL, city)
+    .then((result) => {
+      // look in results for rowCount-----SQL method // that should give me a one,. which tells us that my insert statement worked
+      if (result.rowCount) {
+        console.log("location is in the database");
+        response.status(200).json(result.rows[0]);
+      } else {
+        locationAPIHandler(request.query.city, response);
+      }
+    })
+    .catch((error) => {
+      response.status(500).send(error);
+    });
+  /////send the result to the client
+
+  ////ELSE request data from api
+  //if row is found, send it to the browser
+
+  // if (locations[request.query.city]) {
+  //   console.log("we have it already...");
+  //   response.status(200).send(locations[request.query.city]);
+  // } else {
+  //   console.log("going to get it");
+  //   locationAPIHandler(request.query.city, response);
+  // }
+}
+
+// location api handler
+function locationAPIHandler(city, response) {
   const API = "https://us1.locationiq.com/v1/search.php";
-  // Query String
-  // ?key=${process.env.GEOCODE_API_KEY}&q=${request.query.city}&format=json`;
 
   let queryObject = {
     key: process.env.GEOCODE_API_KEY,
@@ -105,23 +122,26 @@ function fetchLocationDataFromAPI(city, response) {
     .query(queryObject)
     .then((data) => {
       let locationObj = new Location(data.body[0], city);
-
       // save the city for later
-      locations[city] = locationObj;
+
+      ////TODO: this is where insert statement goes.
+      // else we want to look it up in the api and send it to the database (insert into)
+
+      // locations[city] = locationObj; ///// changing this to be the cache
 
       // send the city to the user
       response.status(200).send(locationObj);
     })
     .catch(() => {
-      response.status(500).send(console.log("this is not working "));
+      response.status(500).send(console.log("The location is not working "));
     });
 }
 
 function Location(obj, city) {
+  this.search_query = city;
+  this.formatted_query = obj.display_name;
   this.latitude = obj.lat;
   this.longitude = obj.lon;
-  this.formatted_query = obj.display_name;
-  this.search_query = city;
 }
 
 // Volatile Data -- because it changes frequently, we don't cache it.
